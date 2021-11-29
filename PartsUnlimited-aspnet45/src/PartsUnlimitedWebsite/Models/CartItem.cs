30dfd2d6e0bab8ae57344659463def64431a02b6
Miguel Paredes <Miguel.Paredes@ust.com>
2021-11-29 10:21:49 +0100

    commit de cartltem
    Signed-off-by: Miguel Paredes <Miguel.Paredes@ust.com>

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 namespace PartsUnlimited.Models
 {
     public class CartItem : ILineItem
-    {  // comenzamos la tarea
+    {  // comenzamos la tarea - segundo cambio
         [Key]
         public int CartItemId { get; set; }
 
